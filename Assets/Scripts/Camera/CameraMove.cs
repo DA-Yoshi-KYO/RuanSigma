4643c25429dbca3a069a90177101247c90a25045
Ruan-3144 <esapamimishi@gmail.com>
2025-09-23 03:57:34 +0900

    カメラ処理
    カメラ処理
    プレイヤー中心に回転

--- Assets/Scripts/Camera/CameraMove.cs
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -4,35 +4,34 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    GameObject player;
     public float Mouse = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject player = GameObject.Find("Player");
 
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
-        Vector3 pos = player.transform.position;
+        //プレイヤーを中心にカメラ回転
+        transform.RotateAround(player.transform.position, Vector3.up, mouseX * Mouse);
+        transform.RotateAround(player.transform.position, transform.right, mouseY * Mouse);
 
-        transform.RotateAround(pos, Vector3.up, mouseX * Mouse);
-
-        transform.RotateAround(pos, transform.right, mouseY * Mouse);
-
-        pos += transform.forward * -2.5f;
-
-        pos.y += 1.5f;
-
-        pos.z -= 2.5f;
-        pos.y += 0.5f;
-
-        transform.position = pos;
+        //固定のカメラ回転
+        //Vector3 pos = player.transform.position;
+        //transform.RotateAround(pos, Vector3.up, mouseX * Mouse);
+        //transform.RotateAround(pos, transform.right, mouseY * Mouse);
+        //pos += transform.forward * -2.5f;
+        //pos.y += 1.5f;
+        //pos.z -= 2.5f;
+        //pos.y += 0.5f;
+        //transform.position = pos;
     }
 }
